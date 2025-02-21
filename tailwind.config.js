26810b848468079dbde9eb5feecacb06432a6ecb
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-02-21 15:12:38 +0530

    clear the vercel error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  purge: false, // Add this line if using Tailwind CSS v2.x
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
